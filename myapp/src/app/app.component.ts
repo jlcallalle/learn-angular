2e52bcaaee2ddd71d787a71de168298829c39054
jorge <jcallalle@minsa.gob.pe>
2020-04-05 18:44:57 -0500

    proyecto angular

---
+++ myapp/src/app/app.component.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+import { Product } from './product.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'myapp';
+  items = ['nicolas', 'julian', 'sandra'];
+
+  objeto = {};
+
+  // Para tipar nuestro array con Products
+
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/hoodie.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/images/pin.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/images/stickers1.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'assets/images/stickers2.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+  ];
+
+
+
+  addItem() {
+    console.log('add');
+    this.items.push('nuevo Item');
+  }
+
+  deleteItem(index: number) {
+    console.log('delete');
+    this.items.splice(index, 1);
+  }
+
+}
